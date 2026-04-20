d7130012d81adb0fd73bab70f6ba507905b950c4
dianaramm <dnuniviana@gmail.com>
2026-04-20 14:30:49 -0600

    validación de nombre

--- src/exceptions/datosInvalidosException.java
+++ src/exceptions/datosInvalidosException.java
@@ -2,6 +2,19 @@ package exceptions;
 
 public class datosInvalidosException extends Exception{
     //Diana
+    public static void validarNombre(String nombre) throws datosInvalidosException {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new datosInvalidosException("El nombre no puede estar vacíom vuelve a intentarlo");
+        }
+        if (!nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")) {
+            throw new datosInvalidosException("El nombre solo debe contener letras, vuelve a intentarlo");
+        }
+    }
+
+    // Constructor
+    public datosInvalidosException(String mensaje) {
+        super(mensaje);
+    }
 
 
 
